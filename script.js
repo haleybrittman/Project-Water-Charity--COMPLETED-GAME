7267cb751963a25e4f643cb27cbfc98f82921014
haleybritt <hbrittman@hawk.iit.edu>
2025-10-19 20:28:52 -0500

    updating jerry can path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 🎮 DOM ELEMENTS
 // Player image for jerry can
 const playerImg = new Image();
-playerImg.src = "jerrycan2.png";
+playerImg.src = "jerrycan.png";
 // Preserve aspect ratio when sizing the player image
 playerImg.onload = () => {
   const ratio = playerImg.naturalHeight / playerImg.naturalWidth || 1;
